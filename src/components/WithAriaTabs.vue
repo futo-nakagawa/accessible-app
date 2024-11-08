dfb61bceb2ecf0985009c0d677324ba722be7c82
futo-nakagawa <2210245@ecc.ac.jp>
2024-11-08 11:56:43 +0900

    tabコンポーネント

---
+++ src/components/WithAriaTabs.vue
@@ -0,0 +1,31 @@
+<template>
+    <div role="tablist">
+      <button role="tab" :aria-selected="selectedTab === 1" @click="selectTab(1)">タブ1</button>
+      <button role="tab" :aria-selected="selectedTab === 2" @click="selectTab(2)">タブ2</button>
+      <div v-if="selectedTab === 1">タブ1の内容</div>
+      <div v-if="selectedTab === 2">タブ2の内容</div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        selectedTab: 1,
+      };
+    },
+    methods: {
+      selectTab(tab) {
+        this.selectedTab = tab;
+      },
+    },
+  };
+  </script>
+  
+  <style>
+  button {
+    margin: 5px;
+    padding: 5px 10px;
+  }
+  </style>
+  
---
+++ src/components/WithoutAriaTabs.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+      <button @click="selectTab(1)">タブ1</button>
+      <button @click="selectTab(2)">タブ2</button>
+      <div v-if="selectedTab === 1">タブ1の内容</div>
+      <div v-if="selectedTab === 2">タブ2の内容</div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        selectedTab: 1,
+      };
+    },
+    methods: {
+      selectTab(tab) {
+        this.selectedTab = tab;
+      },
+    },
+  };
+  </script>
+  
+  <style>
+  button {
+    margin: 5px;
+    padding: 5px 10px;
+  }
+  </style>
+  
