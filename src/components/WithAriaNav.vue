18fe1a93234763c4d9f3b869aacb224c3a0246ef
futo-nakagawa <2210245@ecc.ac.jp>
2024-11-08 11:56:27 +0900

    Navコンポーネント

---
+++ src/components/WithAriaNav.vue
@@ -0,0 +1,19 @@
+<template>
+    <nav aria-label="メインナビゲーション">
+      <ul>
+        <li><a href="#home" aria-current="page">ホーム</a></li>
+        <li><a href="#about">概要</a></li>
+        <li><a href="#contact">お問い合わせ</a></li>
+      </ul>
+    </nav>
+  </template>
+  
+  <style>
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  li {
+    margin: 5px 0;
+  }
+  </style>
---
+++ src/components/WithoutAriaNav.vue
@@ -0,0 +1,20 @@
+<template>
+    <nav>
+      <ul>
+        <li><a href="#home">ホーム</a></li>
+        <li><a href="#about">概要</a></li>
+        <li><a href="#contact">お問い合わせ</a></li>
+      </ul>
+    </nav>
+  </template>
+  
+  <style>
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  li {
+    margin: 5px 0;
+  }
+  </style>
+  
