65db82a518ff4d8c7b979e9064708ce8f92bc438
futo-nakagawa <2210245@ecc.ac.jp>
2024-11-08 11:56:11 +0900

    モーダルコンポーネント

---
+++ src/components/WithAriaModal.vue
@@ -0,0 +1,31 @@
+<template>
+    <div v-if="isOpen" role="dialog" aria-labelledby="modalTitle" aria-describedby="modalDesc">
+      <h2 id="modalTitle">モーダルタイトル</h2>
+      <p id="modalDesc">モーダルの説明内容です。</p>
+      <button @click="closeModal">閉じる</button>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        isOpen: true,
+      };
+    },
+    methods: {
+      closeModal() {
+        this.isOpen = false;
+      },
+    },
+  };
+  </script>
+  
+  <style>
+  div {
+    border: 1px solid #000;
+    padding: 20px;
+    margin: 10px;
+  }
+  </style>
+  
---
+++ src/components/WithoutAriaModal.vue
@@ -0,0 +1,31 @@
+<template>
+    <div v-if="isOpen">
+      <h2>モーダルタイトル</h2>
+      <p>モーダルの説明内容です。</p>
+      <button @click="closeModal">閉じる</button>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        isOpen: true,
+      };
+    },
+    methods: {
+      closeModal() {
+        this.isOpen = false;
+      },
+    },
+  };
+  </script>
+  
+  <style>
+  div {
+    border: 1px solid #000;
+    padding: 20px;
+    margin: 10px;
+  }
+  </style>
+  
